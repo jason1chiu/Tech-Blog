719872e82f32c75e285b72c87a85c99150dde72f
jason1chiu <119711904+jason1chiu@users.noreply.github.com>
2023-03-24 22:59:02 -0400

    change the login format

--- public/js/login.js
+++ public/js/login.js
@@ -1,22 +1,31 @@
-async function login(event) {
+const loginForm = document.querySelector('#login-form');
+
+async function loginFormHandler(event) {
   event.preventDefault();
-
-  const email = document.querySelector('#email-login').value.trim();
-  const password = document.querySelector('#password-login').value.trim();
-
+  
+  const email = document.querySelector('#email').value.trim();
+  const password = document.querySelector('#password').value.trim();
+  
   if (email && password) {
-    const response = await fetch('/api/users/login', {
-      method: 'POST',
-      body: JSON.stringify({ email, password }),
-      headers: { 'Content-Type': 'application/json' },
-    });
+    try {
+      const response = await fetch('/api/users/login', {
+        method: 'POST',
+        body: JSON.stringify({ email, password }),
+        headers: { 'Content-Type': 'application/json' }
+      });
 
-    if (response.ok) {
-      document.location.replace('/');
-    } else {
-      alert(response.statusText);
+      if (response.ok) {
+        document.location.replace('/');
+      } else {
+        throw new Error('Invalid login credentials');
+      }
+    } catch (err) {
+      console.error(err);
+      alert('Failed to log in. Please try again.');
     }
   }
 }
 
-document.querySelector('.login-form').addEventListener('submit', login);
+if (loginForm) {
+  loginForm.addEventListener('submit', loginFormHandler);
+}
