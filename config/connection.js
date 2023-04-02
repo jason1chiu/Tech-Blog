5c5a1a83c416643be049eafeefad0f8a9bd19076
jason1chiu <119711904+jason1chiu@users.noreply.github.com>
2023-04-02 12:16:36 -0400

    trying to get this to work

--- config/connection.js
+++ config/connection.js
@@ -1,43 +1,22 @@
 const Sequelize = require('sequelize');
-const url = require('url');
 require('dotenv').config();
 
-// Parse the connection string from Heroku
-const connectionString = process.env.CLEARDB_DATABASE_URL || process.env.JAWSDB_URL;
-const dbUrl = url.parse(connectionString);
+// const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
+//     host: 'localhost',
+//     dialect: 'mysql',
+//     port: 3306
+// });
 
-// Extract the required information
-const dbName = dbUrl.pathname.substring(1);
-const dbUser = dbUrl.auth.split(':')[0];
-const dbPassword = dbUrl.auth.split(':')[1];
-const dbHost = dbUrl.hostname;
-const dbPort = dbUrl.port;
+let sequelize;
 
-const sequelize = new Sequelize(
-  dbName,
-  dbUser,
-  dbPassword,
-  {
-    host: dbHost,
+if (process.env.JAWSDB_URL) {
+  sequelize = new Sequelize(process.env.JAWSDB_URL);
+} else {
+  sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
+    host: '127.0.0.1',
     dialect: 'mysql',
-    port: dbPort,
-  }
-);
+    port: 3306
+  });
+}
 
 module.exports = sequelize;
-
-// const Sequelize = require('sequelize');
-// require('dotenv').config();
-
-// const sequelize = new Sequelize(
-//   process.env.DB_NAME,
-//   process.env.DB_USER,
-//   process.env.DB_PW,
-//   {
-//     host: '127.0.0.1',
-//     dialect: 'mysql',
-//     port: 3306,
-//   }
-// );
-
-// module.exports = sequelize;
