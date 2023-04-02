50924740d501a88008cd9753a0a3a6422ed75632
jason1chiu <119711904+jason1chiu@users.noreply.github.com>
2023-04-02 13:03:34 -0400

    Updateing JAWSDB_URL

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -26,6 +26,7 @@ router.get('/', (req, res) => {
   })
   .then(dbPostData => {
     const posts = dbPostData.map(post => post.get({ plain: true }));
+    console.log(posts); // Add this line to check if the posts are retrieved correctly
     res.render('homepage', { 
       posts, 
       loggedIn: req.session.loggedIn,
